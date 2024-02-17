a73bb437f78a70c38c363c97861c7f519f756a0d
Erno Irwandi <49245597+waeerno@users.noreply.github.com>
2024-02-17 22:02:38 +0700

    suska

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -19,7 +19,7 @@ class UserSeeder extends Seeder
         \App\Models\User::insert([
             [
                 'name' => 'Ahmad Bahri Al Anwar',
-                'email' => 'ahmad.bahri@uin-siska.ac.id',
+                'email' => 'ahmad.bahri@uin-suska.ac.id',
                 'email_verified_at' => now(),
                 'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
                 'gender' => 'male',
