c1f6bd28051b4c29ecc4c7ad6287fef1a74866b9
Erno Irwandi <49245597+waeerno@users.noreply.github.com>
2024-02-18 16:12:59 +0700

    mati komemnt

--- resources/js/pages/login/index.js
+++ resources/js/pages/login/index.js
@@ -39,67 +39,67 @@
         console.log('test');
 
 
-        // axios
-        //     .post(`${baseURL}/client/auth/login`, {
-        //         username: email,
-        //         password: password,
-        //     })
-        //     .then((res) => {
-        //         console.log(res.data);
-        //         if (res.data.status === 1) {
-        //             // Jika login berhasil, simpan data pada cookies
-        //             Cookies.set("token", res.data.data.jwt.token);
-        //             Cookies.set("id_pd", res.data.data.data.id_pd);
-        //             Cookies.set("nm_pd", res.data.data.data.nm_pd);
+        axios
+            .post(`${baseURL}/client/auth/login`, {
+                username: email,
+                password: password,
+            })
+            .then((res) => {
+                console.log(res.data);
+                if (res.data.status === 1) {
+                    // Jika login berhasil, simpan data pada cookies
+                    Cookies.set("token", res.data.data.jwt.token);
+                    Cookies.set("id_pd", res.data.data.data.id_pd);
+                    Cookies.set("nm_pd", res.data.data.data.nm_pd);
 
-        //             window.location = "/mahasiswa-absen";
-        //         } else {
-        //             // Jika login gagal, tampilkan pesan kesalahan dari server
-        //             $("#btn-login").html("Login");
-        //             $("#password").addClass("border-danger");
-        //             $("#error-password").html(res.data.error_msg);
-        //         }
-        //     })
-        //     .catch((err) => {
-        //         $("#btn-login").html("Login");
-        //         if (
-        //             err.response &&
-        //             err.response.data &&
-        //             err.response.data.message
-        //         ) {
-        //             if (
-        //                 err.response.data.message !==
-        //                 "Wrong username or password."
-        //             ) {
-        //                 for (const [key, val] of Object.entries(
-        //                     err.response.data.errors
-        //                 )) {
-        //                     $(`#${key}`).addClass("border-danger");
-        //                     $(`#error-${key}`).html(val);
-        //                 }
-        //             } else {
-        //                 $(`#password`).addClass("border-danger");
-        //                 $(`#error-password`).html(err.response.data.message);
-        //             }
-        //         } else {
-        //             // Penanganan kesalahan lain yang mungkin terjadi
-        //             console.error(err);
+                    window.location = "/mahasiswa-absen";
+                } else {
+                    // Jika login gagal, tampilkan pesan kesalahan dari server
+                    $("#btn-login").html("Login");
+                    $("#password").addClass("border-danger");
+                    $("#error-password").html(res.data.error_msg);
+                }
+            })
+            .catch((err) => {
+                $("#btn-login").html("Login");
+                if (
+                    err.response &&
+                    err.response.data &&
+                    err.response.data.message
+                ) {
+                    if (
+                        err.response.data.message !==
+                        "Wrong username or password."
+                    ) {
+                        for (const [key, val] of Object.entries(
+                            err.response.data.errors
+                        )) {
+                            $(`#${key}`).addClass("border-danger");
+                            $(`#error-${key}`).html(val);
+                        }
+                    } else {
+                        $(`#password`).addClass("border-danger");
+                        $(`#error-password`).html(err.response.data.message);
+                    }
+                } else {
+                    // Penanganan kesalahan lain yang mungkin terjadi
+                    console.error(err);
 
-        //             //kirim pop up error ke view
-        //             Toastify({
-        //                 node: $("#basic-non-sticky-notification-content")
-        //                     .clone()
-        //                     .removeClass("hidden")[0],
-        //                 duration: 3000,
-        //                 newWindow: true,
-        //                 close: true,
-        //                 gravity: "top",
-        //                 position: "right",
-        //                 backgroundColor: "white",
-        //                 stopOnFocus: true,
-        //             }).showToast();
-        //         }
-        //     });
+                    //kirim pop up error ke view
+                    Toastify({
+                        node: $("#basic-non-sticky-notification-content")
+                            .clone()
+                            .removeClass("hidden")[0],
+                        duration: 3000,
+                        newWindow: true,
+                        close: true,
+                        gravity: "top",
+                        position: "right",
+                        backgroundColor: "white",
+                        stopOnFocus: true,
+                    }).showToast();
+                }
+            });
     }
 
     $("#login-form").on("keyup", function (e) {
